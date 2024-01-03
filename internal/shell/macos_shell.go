63567c81e609c84e177dcb76b5e735414932d864
lihan <lihan@apache.org>
2024-01-03 09:45:31 +0800

    Happy New Year!

--- internal/shell/macos_shell.go
+++ internal/shell/macos_shell.go
@@ -1,7 +1,7 @@
 //go:build darwin || linux
 
 /*
- *    Copyright 2023 [lihan aooohan@gmail.com]
+ *    Copyright 2024 [lihan aooohan@gmail.com]
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
