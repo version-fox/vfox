a9f4f037385880f254b8e1dee4e121a8becfb631
lijiacheng.ljc <lijiacheng.ljc@antgroup.com>
2024-03-06 17:19:35 +0800

    fix: fix decode in mixed struct

---
+++ internal/luai/encode.go
@@ -0,0 +1,79 @@
+// Lua Interface
+// Marshal and Unmarshal Lua Table to Go Struct
+
+package luai
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/version-fox/vfox/internal/logger"
+	lua "github.com/yuin/gopher-lua"
+)
+
+func Marshal(state *lua.LState, v any) (lua.LValue, error) {
+	reflected := reflect.ValueOf(v)
+	if reflected.Kind() == reflect.Ptr {
+		reflected = reflected.Elem()
+	}
+	logger.Debug(v, reflected.Kind())
+
+	switch reflected.Kind() {
+	case reflect.Struct:
+		table := state.NewTable()
+		for i := 0; i < reflected.NumField(); i++ {
+			field := reflected.Field(i)
+			fieldType := reflected.Type().Field(i)
+			if field.Kind() == reflect.Ptr {
+				field = field.Elem()
+			}
+
+			tag := fieldType.Tag.Get("luai")
+			if tag == "" {
+				tag = fieldType.Name
+			}
+
+			sub, err := Marshal(state, field.Interface())
+			if err != nil {
+				return nil, err
+			}
+			logger.Debugf("field: %v, tag: %v, sub: %v, kind: %s\n", field, tag, sub, field.Kind())
+			table.RawSetString(tag, sub)
+		}
+		return table, nil
+	case reflect.String:
+		return lua.LString(reflected.String()), nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return lua.LNumber(reflected.Int()), nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return lua.LNumber(reflected.Uint()), nil
+	case reflect.Float32, reflect.Float64:
+		return lua.LNumber(reflected.Float()), nil
+	case reflect.Bool:
+		return lua.LBool(reflected.Bool()), nil
+	case reflect.Array, reflect.Slice:
+		table := state.NewTable()
+		for i := 0; i < reflected.Len(); i++ {
+			value, err := Marshal(state, reflected.Index(i).Interface())
+			if err != nil {
+				return nil, err
+			}
+			table.RawSetInt(i+1, value)
+		}
+		return table, nil
+	case reflect.Map:
+		table := state.NewTable()
+		for _, key := range reflected.MapKeys() {
+			value, err := Marshal(state, reflected.MapIndex(key).Interface())
+			if err != nil {
+				return nil, err
+			}
+
+			table.RawSetString(key.String(), value)
+		}
+		return table, nil
+	default:
+		return nil, errors.New("marshal: unsupported type " + reflected.Kind().String() + " for reflected ")
+	}
+
+}
--- internal/luai/encoding_test.go
+++ internal/luai/encoding_test.go
@@ -1,10 +1,38 @@
 package luai
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
+	"github.com/version-fox/vfox/internal/logger"
 	lua "github.com/yuin/gopher-lua"
 )
+
+const preloadScript = `
+function printTable(t, indent)
+    indent = indent or 0
+    local strIndent = string.rep("  ", indent)
+    for key, value in pairs(t) do
+        local keyStr = tostring(key)
+        local valueStr = tostring(value)
+        if type(value) == "table" then
+            print(strIndent .. "[" .. keyStr .. "] =>")
+            printTable(value, indent + 1)
+        else
+            print(strIndent .. "[" .. keyStr .. "] => " .. valueStr)
+        end
+    end
+end
+`
+
+func setupSuite(tb testing.TB) func(tb testing.TB) {
+	logger.SetLevel(logger.DebugLevel)
+
+	return func(tb testing.TB) {
+		logger.SetLevel(logger.InfoLevel)
+	}
+}
 
 type testStruct struct {
 	Field1 string
@@ -14,6 +42,7 @@ type testStruct struct {
 
 func TestRegular(t *testing.T) {
 	luaVm := lua.NewState()
+	defer luaVm.Close()
 
 	test := testStruct{
 		Field1: "test",
@@ -61,16 +90,8 @@ func TestRegular(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if struct2.Field1 != "test" {
-		t.Errorf("expected 'test', got '%s'", struct2.Field1)
-	}
-
-	if struct2.Field2 != 1 {
-		t.Errorf("expected 1, got %d", struct2.Field2)
-	}
-
-	if struct2.Field3 != true {
-		t.Errorf("expected true, got %t", struct2.Field3)
+	if !reflect.DeepEqual(test, struct2) {
+		t.Errorf("expected %+v, got %+v", test, struct2)
 	}
 }
 
@@ -82,6 +103,7 @@ type testStructTag struct {
 
 func TestTag(t *testing.T) {
 	luaVm := lua.NewState()
+	defer luaVm.Close()
 
 	test := testStructTag{
 		Field1: "test",
@@ -129,15 +151,196 @@ func TestTag(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if struct2.Field1 != "test" {
-		t.Errorf("expected 'test', got '%s'", struct2.Field1)
+	if !reflect.DeepEqual(test, struct2) {
+		t.Errorf("expected %+v, got %+v", test, struct2)
+	}
+}
+
+func TestMapAndSlice(t *testing.T) {
+	teardownSuite := setupSuite(t)
+	defer teardownSuite(t)
+
+	// Test case for map
+	m := map[string]interface{}{
+		"key1": "value1",
+		"key2": 2,
+		"key3": true,
+	}
+	L := lua.NewState()
+	defer L.Close()
+	table, err := Marshal(L, m)
+	if err != nil {
+		t.Fatalf("marshal map failed: %v", err)
+	}
+	L.SetGlobal("m", table)
+	if err := L.DoString(`
+		assert(m.key1 == "value1")
+		assert(m.key2 == 2)
+		assert(m.key3 == true)
+	`); err != nil {
+		t.Errorf("map test failed: %v", err)
 	}
 
-	if struct2.Field2 != 1 {
-		t.Errorf("expected 1, got %d", struct2.Field2)
+	// Test case for slice
+	s := []any{"value1", 2, true}
+
+	slice, err := Marshal(L, s)
+	if err != nil {
+		t.Fatalf("marshal slice failed: %v", err)
 	}
 
-	if struct2.Field3 != true {
-		t.Errorf("expected true, got %t", struct2.Field3)
+	L.SetGlobal("s", slice)
+	if err := L.DoString(`
+		assert(s[1] == "value1")
+		assert(s[2] == 2)
+		assert(s[3] == true)
+	`); err != nil {
+		t.Errorf("slice test failed: %v", err)
 	}
+
+	// Unmarshal
+
+	// Test case for map
+	m2 := map[string]any{}
+
+	fmt.Println("==== start unmarshal ====")
+
+	err = Unmarshal(table, &m2)
+	if err != nil {
+		t.Fatalf("unmarshal map failed: %v", err)
+	}
+
+	fmt.Printf("m2: %+v\n", m2)
+	if m2["key1"] != "value1" {
+		t.Errorf("expected value1, got %v", m2["key1"])
+	}
+	if m2["key2"] != 2 {
+		t.Errorf("expected 2, got %v", m2["key2"])
+	}
+	if m2["key3"] != true {
+		t.Errorf("expected true, got %v", m2["key3"])
+	}
+
+	// Test case for slice
+	s2 := []any{}
+
+	err = Unmarshal(slice, &s2)
+	if err != nil {
+		t.Fatalf("unmarshal slice failed: %v", err)
+	}
+
+	fmt.Printf("s2: %+v\n", s2)
+
+	if s2[0] != "value1" {
+		t.Errorf("expected value1, got %v", s2[0])
+	}
+	if s2[1] != 2 {
+		t.Errorf("expected 2, got %v", s2[1])
+	}
+	if s2[2] != true {
+		t.Errorf("expected true, got %v", s2[2])
+	}
+}
+
+type complexStruct struct {
+	Field1 string
+	Field2 int
+	Field3 bool
+	Map    map[string]interface{}
+	Slice  []any
+}
+
+func TestMapSliceStructUnified(t *testing.T) {
+	teardownSuite := setupSuite(t)
+	defer teardownSuite(t)
+
+	m := map[string]interface{}{
+		"key1": "value1",
+		"key2": 2,
+		"key3": true,
+	}
+
+	s := []any{"value1", 2, true}
+
+	t.Run("TestCase1", func(t *testing.T) {
+		L := lua.NewState()
+		defer L.Close()
+
+		input := complexStruct{
+			Field1: "value1",
+			Field2: 123,
+			Field3: true,
+			Map:    m,
+			Slice:  s,
+		}
+
+		table, err := Marshal(L, input)
+		if err != nil {
+			t.Fatalf("marshal map failed: %v", err)
+		}
+
+		L.SetGlobal("m", table)
+
+		L.DoString(preloadScript)
+
+		if err := L.DoString(`
+			assert(m.Field1 == "value1")
+			assert(m.Field2 == 123)
+			assert(m.Field3 == true)
+			assert(m.Map.key1 == "value1")
+			assert(m.Map.key2 == 2)
+			assert(m.Map.key3 == true)
+			assert(m.Slice[1] == "value1")
+			assert(m.Slice[2] == 2)
+			assert(m.Slice[3] == true)
+			printTable(m)
+			`); err != nil {
+			t.Errorf("map test failed: %v", err)
+		}
+
+		// Unmarshal
+		output := complexStruct{}
+		err = Unmarshal(table, &output)
+		if err != nil {
+			t.Fatalf("unmarshal map failed: %v", err)
+		}
+
+		isEqual := reflect.DeepEqual(input, output)
+		if !isEqual {
+			t.Fatalf("expected %+v, got %+v", input, output)
+		}
+
+		fmt.Printf("output: %+v\n", output)
+		if output.Field1 != "value1" {
+			t.Errorf("expected value1, got %v", output.Field1)
+		}
+
+		if output.Field2 != 123 {
+			t.Errorf("expected 123, got %v", output.Field2)
+		}
+
+		if output.Field3 != true {
+			t.Errorf("expected true, got %v", output.Field3)
+		}
+
+		if output.Map["key1"] != "value1" {
+			t.Errorf("expected value1, got %v", output.Map["key1"])
+		}
+
+		if output.Map["key2"] != 2 {
+			t.Errorf("expected 2, got %v", output.Map["key2"])
+		}
+
+		if output.Map["key3"] != true {
+			t.Errorf("expected true, got %v", output.Map["key3"])
+		}
+
+		if output.Slice[0] != "value1" {
+			t.Errorf("expected value1, got %v", output.Slice[0])
+		}
+
+		if output.Slice[1] != 2 {
+			t.Errorf("expected 2, got %v", output.Slice[1])
+		}
+	})
 }
