5032a41d7e35f06d46db5e7767b0b87cc4cb8844
lihan <lihan@apache.org>
2024-04-26 11:21:44 +0800

    add cache

---
+++ internal/cache/cache.go
@@ -0,0 +1,135 @@
+/*
+ *    Copyright 2024 Han Li and contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package cache
+
+import (
+	"encoding/gob"
+	"encoding/json"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	NeverExpired time.Duration = -1
+)
+
+// Value CacheValue is a byte slice that can be unmarshaled into any type
+type Value []byte
+
+// Unmarshal the CacheValue into the given value
+func (c Value) Unmarshal(v any) error {
+	return json.Unmarshal(c, v)
+}
+
+// NewValue marshals the given value into a CacheValue
+func NewValue(v any) (Value, error) {
+	return json.Marshal(v)
+}
+
+// Item is a cache item
+type Item struct {
+	Val    []byte
+	Expire int64 // Expire time in UnixNano, -1 means never expire
+}
+
+// FileCache is a cache that saves to a file
+type FileCache struct {
+	mu    sync.RWMutex
+	items map[string]Item
+	path  string
+}
+
+// NewFileCache creates a new FileCache
+func NewFileCache(path string) (*FileCache, error) {
+	fc := &FileCache{
+		items: make(map[string]Item),
+		path:  path,
+	}
+	if err := fc.loadFromFile(path); err != nil {
+		return nil, err
+	}
+	return fc, nil
+}
+
+// Set a key value pair with a duration
+func (c *FileCache) Set(key string, value Value, duration time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if duration == NeverExpired {
+		c.items[key] = Item{
+			Val:    value,
+			Expire: int64(NeverExpired),
+		}
+	} else {
+		c.items[key] = Item{
+			Val:    value,
+			Expire: time.Now().Add(duration).UnixNano(),
+		}
+	}
+}
+
+// Get a value by key
+func (c *FileCache) Get(key string) (Value, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	item, exists := c.items[key]
+	if !exists || time.Now().UnixNano() > item.Expire {
+		if item.Expire != int64(NeverExpired) {
+			// Remove expired item
+			delete(c.items, key)
+			return nil, false
+		}
+	}
+	return item.Val, true
+}
+
+// Remove a key from the cache
+func (c *FileCache) Remove(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.items, key)
+}
+
+// Close the cache and save to file
+func (c *FileCache) Close() error {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	file, err := os.Create(c.path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	return encoder.Encode(c.items)
+}
+
+// loadFromFile loads the cache from a file
+func (c *FileCache) loadFromFile(filename string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	return decoder.Decode(&c.items)
+}
---
+++ internal/cache/cache_test.go
@@ -0,0 +1,125 @@
+package cache
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+type kv struct {
+	key   string
+	value Value
+}
+
+type test1 struct {
+	V1 string
+	V2 int
+	V3 bool
+}
+
+func newKv(t *testing.T, v any) Value {
+	value, err := NewValue(v)
+	if err != nil {
+		t.Fatalf("Failed to create value: %v", err)
+	}
+	return value
+}
+
+func TestFileCache(t *testing.T) {
+	cache, err := NewFileCache("testfile.cache")
+	if err != nil {
+		t.Errorf("Failed to create cache: %v", err)
+	}
+	testdata := []kv{
+		{"test1", Value("test1")},
+		{"test2", newKv(t, true)},
+		{"test3", newKv(t, 123)},
+		{"test4", newKv(t, 'c')},
+	}
+	t.Run("TestSimpleType", func(t *testing.T) {
+
+		for _, d := range testdata {
+			cache.Set(d.key, d.value, NeverExpired)
+		}
+
+		for _, d := range testdata {
+			v, ok := cache.Get(d.key)
+			if !ok {
+				t.Errorf("Failed to get key %s", d.key)
+			}
+			if string(v) != string(d.value) {
+				t.Errorf("Expected %s, got %s", string(d.value), string(v))
+			}
+		}
+	})
+
+	t.Run("TestStructType", func(t *testing.T) {
+		td1 := test1{
+			V1: "test",
+			V2: 123,
+			V3: true,
+		}
+		cache.Set("test5", newKv(t, td1), NeverExpired)
+
+		v, ok := cache.Get("test5")
+		if !ok {
+			t.Errorf("Failed to get key test5")
+		}
+		result := test1{}
+		if err = v.Unmarshal(&result); err != nil {
+			t.Errorf("Failed to unmarshal: %v", err)
+		}
+		if result.V1 != td1.V1 || result.V2 != td1.V2 || result.V3 != td1.V3 {
+			t.Errorf("Expected %v, got %v", td1, result)
+		}
+	})
+
+	t.Run("TestExpire", func(t *testing.T) {
+		el := len(cache.items)
+		cache.Set("test6", newKv(t, "123"), time.Second)
+		time.Sleep(time.Second * 2)
+
+		_, ok := cache.Get("test6")
+		if ok {
+			t.Errorf("Expected key test6 to be expired")
+		}
+		if len(cache.items) != el {
+			t.Errorf("Expected %d items, got %d", el, len(cache.items))
+		}
+	})
+
+	t.Run("TestRemove", func(t *testing.T) {
+		cache.Set("test7", newKv(t, "123"), NeverExpired)
+		cache.Remove("test7")
+		_, ok := cache.Get("test7")
+		if ok {
+			t.Errorf("Expected key test7 to be removed")
+		}
+	})
+
+	t.Run("TestToFile", func(t *testing.T) {
+		for _, d := range testdata {
+			cache.Set(d.key, d.value, NeverExpired)
+		}
+
+		if err := cache.Close(); err != nil {
+			t.Errorf("Failed to close cache: %v", err)
+		}
+
+		newCache, err := NewFileCache("testfile.cache")
+		if err != nil {
+			t.Errorf("Failed to create cache: %v", err)
+		}
+		for _, d := range testdata {
+			v, ok := newCache.Get(d.key)
+			if !ok {
+				t.Errorf("Failed to get key %s", d.key)
+			}
+			if string(v) != string(d.value) {
+				t.Errorf("Expected %s, got %s", string(d.value), string(v))
+			}
+		}
+	})
+
+	defer os.Remove("testfile.cache")
+}
